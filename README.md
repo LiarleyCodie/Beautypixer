5491ea50918e7e3e4b6b3f39de624ea221b9dd47
liarleycodie <liarleyyt@gmail.com>
2024-05-28 16:49:23 -0300

    init: [0] initial setup created

---
+++ README.md
@@ -0,0 +1,11 @@
+# Beautypixer 🦋
+
+✨ É uma extensao de navegador inspirada pelo [Momentum](https://momentumdash.com/) e [Pexels](https://chromewebstore.google.com/detail/pexels/fdnpgodccdfgofepkclnopmbnacjkbnj)
+com o diferencial de ser completamente gratuita e mesclar algumas coisas de ambos.
+
+🛠️ Essa extensão foi desenvolvida usando:
+- [Svelte](https://svelte.dev/): Framework para desenvolver a interface
+- [Vite + TS](https://vitejs.dev/): Bundler
+- [DM Sans](https://fonts.google.com/specimen/DM+Sans): Familia de fontes
+
+💖 e muito amor e carinho envolvido.
---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+import './app.css'
+
+const imageUrl = 'https://images.unsplash.com/photo-1552486289-7a93ac6d69a5?q=80&w=1920'
+</script>
+
+<div class='main' style='background-image: url({imageUrl})'>
+  <h1 class='h1'>Beautypixer</h1>
+  <p>Hi, welcome to the grass land.</p>
+</div>
+
+<style lang='scss'>
+.main {
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: .4rem;
+
+  background-size: cover;
+  background-position: center;
+
+  position: relative;
+
+  color: white;
+}
+
+.h1 {
+  font-size: 4.8rem;
+  line-height: 1;
+}
+</style>
